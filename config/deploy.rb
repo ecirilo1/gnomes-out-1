1b446815f5e7f639369ad635e81551f05b9af674
Estelle Cirilo <estelle.cirilo@gmail.com>
2021-10-18 10:29:36 +0200

    add gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,13 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 set :keep_releases, 5
 set :default_env, { home: "~/.rbenv/bin/rbenv init:$HOME" }
+# config/deploy.rb
 
+set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
+set :rbenv_ruby, '2.4.2'
 # task :set_remote_rbenv_path do
 # set :rbenv_path, '/home/deploy/.rbenv/bin/rbenv'
 # end
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
